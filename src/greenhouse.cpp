5c732877d7df6e6b42a1e4c1f16d0184f59bfe58
Magnus Kjaedegaard <magnus@kjaedegaard.dk>
2021-05-12 15:10:47 +0200

    actuators drawing v.2

--- src/greenhouse.cpp
+++ src/greenhouse.cpp
@@ -48,9 +48,9 @@ int main(int argc, char const *argv[])
     std::vector<double> tomato_temp_conditions{22,25};
 
     // create vectors 
-    std::vector<float> plant1_position{150,365};
-    std::vector<float> plant2_position{300,365};
-    std::vector<float> plant3_position{450,365};
+    std::vector<float> plant1_position{300,365};
+    std::vector<float> plant2_position{450,365};
+    
 
     //create tomato plant 1
     Tomato_plant plant1(tomato_hum_conditions, tomato_temp_conditions, plant1_position);
@@ -60,10 +60,6 @@ int main(int argc, char const *argv[])
     Tomato_plant plant2(tomato_hum_conditions, tomato_temp_conditions, plant2_position);
     plant2.plant_tomato();
 
-    //create tomato plant 3
-    Tomato_plant plant3(tomato_hum_conditions, tomato_temp_conditions, plant3_position);
-    plant3.plant_tomato();
-
     // initialize the font by loading the font file placed in the build file
     sf::Font font;
     if(!font.loadFromFile("Digitalt.ttf")){
@@ -71,25 +67,17 @@ int main(int argc, char const *argv[])
     }
     
     // create vector with coordanates to use as refference in diagram class
-    std::vector<float> diagram_pos{650,280};
+    std::vector<float> diagram1_pos{50,280};
+    std::vector<float> diagram2_pos{650,280};
 
     // Draw title
-    sf::Text title;
-    title.setFont(font);
-    title.setString("Current\nconditions:");
-    title.setCharacterSize(25);
-    title.setFillColor(sf::Color::White);
-    title.setPosition(sf::Vector2f{diagram_pos[0]-10,diagram_pos[1]-280});
+    sf::Text title1;
+    title1.setFont(font);
+    title1.setString("Current\nconditions:");
+    title1.setCharacterSize(25);
+    title1.setFillColor(sf::Color::White);
+    title1.setPosition(sf::Vector2f{diagram1_pos[0]-10,diagram1_pos[1]-280});
 
-    // Create diagrams displaying conditions, one for each sensor/regulator pair
-    Diagram humdiagram(diagram_pos,2,std::string( "H\nu\nm\ni\nd\ni\nt\ny"), sf::Color::Green,std::string("%"));
-    Diagram tempdiagram(diagram_pos,3,std::string( "T\ne\nm\np\ne\nr\na\nt\nu\nr\ne"), sf::Color::Red, std::string("C"));
-    Diagram livcondiagram(diagram_pos,1,std::string("L\ni\nv\ni\nn\ng\n \nc\no\nn\nd\ni\nt\ni\no\nn\ns\n"), sf::Color::Blue, std::string("%"));
-    
-    // create vector with coordanates to use as refference in diagram1 class
-    std::vector<float> diagram2_pos{50,280};
-
-    // Draw title
     sf::Text title2;
     title2.setFont(font);
     title2.setString("Current\nconditions 1:");
@@ -98,12 +86,17 @@ int main(int argc, char const *argv[])
     title2.setPosition(sf::Vector2f{diagram2_pos[0]-10,diagram2_pos[1]-280});
 
     // Create diagrams displaying conditions, one for each sensor/regulator pair
+    Diagram humdiagram1(diagram1_pos,2,std::string( "H\nu\nm\ni\nd\ni\nt\ny"), sf::Color::Green,std::string("%"));
+    Diagram tempdiagram1(diagram1_pos,3,std::string( "T\ne\nm\np\ne\nr\na\nt\nu\nr\ne"), sf::Color::Red, std::string("C"));
+    Diagram livcondiagram1(diagram1_pos,1,std::string("L\ni\nv\ni\nn\ng\n \nc\no\nn\nd\ni\nt\ni\no\nn\ns\n"), sf::Color::Blue, std::string("%"));
+    
+    // Create diagrams displaying conditions, one for each sensor/regulator pair
     Diagram humdiagram2(diagram2_pos,2,std::string( "H\nu\nm\ni\nd\ni\nt\ny"), sf::Color::Green,std::string("%"));
     Diagram tempdiagram2(diagram2_pos,3,std::string( "T\ne\nm\np\ne\nr\na\nt\nu\nr\ne"), sf::Color::Red, std::string("C"));
     Diagram livcondiagram2(diagram2_pos,1,std::string("L\ni\nv\ni\nn\ng\n \nc\no\nn\nd\ni\nt\ni\no\nn\ns\n"), sf::Color::Blue, std::string("%"));
 
     // Create humidity1 loop objects
-    Condition humidity1(33,0.4);
+    Condition humidity1(33,0.5);
     SensorSim humidity_sensor1;
     Regulator humidity_regulator1(plant1.get_opt_hum_conditions());
     Humidifier humidifier1(greenhouse_info, 1, 1);
@@ -112,7 +105,7 @@ int main(int argc, char const *argv[])
     // loop will look like condition_loop(humidifier, dehumidifier, humidity, humidity_sensor, humidity_regulator)
 
     // create temperature1 loop objects
-    Condition temperature1(18, 0.4);
+    Condition temperature1(18, 0.5);
     SensorSim temperature_sensor1;
     Regulator temperature_regulator1(plant1.get_opt_temp_conditions());
     Cooler cooler1(greenhouse_info, 1, 3);
@@ -137,7 +130,8 @@ int main(int argc, char const *argv[])
 
     // loop will look like: condition_loop(heater, cooler, temperature, temperature_sensor, temperature_regulator)
 
-    std::vector<int> reg_states;
+    std::vector<int> reg_states1;
+    std::vector<int> reg_states2;
 
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Tomato simulator");
@@ -157,16 +151,23 @@ int main(int argc, char const *argv[])
         }
 
         //condition loops
-        condition_loop(humidifier, dehumidifier, humidity, humidity_sensor, humidity_regulator);
-        condition_loop(heater, cooler, temperature, temperature_sensor, temperature_regulator);
+        condition_loop(humidifier1, dehumidifier1, humidity1, humidity_sensor1, humidity_regulator1);
+        condition_loop(heater1, cooler1, temperature1, temperature_sensor1, temperature_regulator1);
+
+        condition_loop(humidifier2, dehumidifier2, humidity2, humidity_sensor2, humidity_regulator2);
+        condition_loop(heater2, cooler2, temperature2, temperature_sensor2, temperature_regulator2);
 
         //update plant
-        reg_states.clear();
-        reg_states.push_back(humidity_regulator.get_state());
-        reg_states.push_back(temperature_regulator.get_state());
-        plant1.cal_living_conditions(reg_states);
-        plant2.cal_living_conditions(reg_states);
-        plant3.cal_living_conditions(reg_states);
+        reg_states1.clear();
+        reg_states1.push_back(humidity_regulator1.get_state());
+        reg_states1.push_back(temperature_regulator1.get_state());
+        plant1.cal_living_conditions(reg_states1);
+
+        reg_states2.clear();
+        reg_states2.push_back(humidity_regulator2.get_state());
+        reg_states2.push_back(temperature_regulator2.get_state());
+        plant2.cal_living_conditions(reg_states2);
+       
 
         // clear the window with black color
         window.clear(sf::Color::Black);
